66d6ce9ffa5533afb3086497a9887882aa719048
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-04 10:30:35 +0200

    H

--- PrcticalloopsArrays/Employee.java
+++ PrcticalloopsArrays/Employee.java
@@ -1,5 +1,23 @@
 package PrcticalloopsArrays;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Employee {
+
+    public static void main(String[] args) {
+
+        String dayWeek[] = new String[7];
+        String[] month;
+        month = new String[12];
+
+        dayWeek[0] = "Monday";
+        month[1] = "December";
+
+        System.out.println(dayWeek);
+        Arrays.toString(dayWeek);
+
+
+    }
 
 }
