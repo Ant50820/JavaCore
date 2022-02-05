7486e8d4916f4f02e190184c6cd7d908d02bf9f2
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-05 20:26:01 +0200

    practical

---
+++ PrcticalloopsArrays/ArrayClass.java
@@ -0,0 +1,72 @@
+package PrcticalloopsArrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayClass {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+//        String[] carsMod = {"Ford", "Opel", "Toyota", "BMV"};
+//        for (String h : carsMod) {
+//            System.out.println(h);
+//        }
+
+//        int[] numbers = {5, 4, 9,4,8};
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println( i + ")" + numbers[i]);
+//
+//        }
+
+
+//        for (int i = 0; i < args.length; i++) {
+//            String arg = args[i];
+//            System.out.println(arg);
+//            System.out.println(i);
+//
+//        }
+//
+//        int i = 0;
+//
+//        do {
+//            System.out.println(i + " + " + " 1 " +  " = " + ++i);
+//        }
+//        while (i < 5);
+
+
+
+
+//
+//        int num = 0;
+//        while (num <= 5) {
+//            System.out.println(num + ") " + num);
+//            num++;
+//        }
+
+
+
+
+
+//        String dayWeek[] = new String[7];
+//
+//        String[] month;
+//        month = new String[12];
+//
+//        dayWeek[0] = "Monday";
+//        month[11] = "December";
+//
+//        String[] days = {"Mon", "Tus", "Wen", "Thus"};
+//        System.out.println(Arrays.toString(days));
+//        System.out.println(Arrays.toString(dayWeek));
+//        System.out.println(Arrays.toString(month));
+//
+//        int[] monthDays = {31, 10, 15};
+
+
+    }
+}
