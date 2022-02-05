89f57c9082b9d4e70c95689802cc53dc75adc3c2
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-06 00:00:35 +0200

    practical task 1

--- PracticalTaskLesson6/Car.java
+++ PracticalTaskLesson6/Car.java
@@ -3,6 +3,7 @@ package PracticalTaskLesson6;
 public abstract class Car {
 
     public String models;
+    public int maxSpeed;
     public int yearOfManufacture;
 
     public abstract void run();
@@ -11,10 +12,25 @@ public abstract class Car {
 }
 
 class Truck extends Car {
+
+    public Truck(String models, int maxSpeed, int yearOfManufacture) {
+        super();
+        this.models = models;
+        this.maxSpeed = maxSpeed;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
     public void run() {};
     public void stop(){};
 }
 class Sedan extends Car {
+
+    public Sedan(String models, int maxSpeed, int yearOfManufacture) {
+        super();
+        this.models = models;
+        this.maxSpeed = maxSpeed;
+        this.yearOfManufacture = yearOfManufacture;
+    }
     public void run() {};
     public void stop(){};
 }
--- PracticalTaskLesson6/Main.java
+++ PracticalTaskLesson6/Main.java
@@ -1,12 +1,54 @@
 package PracticalTaskLesson6;
 
+import java.util.Arrays;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
 
+        Car[] carTrucks = new Car[4];
+        carTrucks[0] = new Truck("Ford", 180, 1980);
+        carTrucks[1] = new Truck("Toyota", 150, 1960);
+        carTrucks[2] = new Truck("Fiat", 190, 1980);
+        carTrucks[3] = new Truck("BMV", 200, 1969);
+
+        Truck truck = new Truck("Honda", 200, 1980);
 
 
-        Truck[] trucks = new Truck[5];
-        Sedan[] sedans = new Sedan[5];
+        for (int i = 0; i < carTrucks.length; i++) {
+            Car carTruck = carTrucks[i];
+            System.out.println(carTruck.models + " - " + carTruck.maxSpeed + " - " + carTruck.yearOfManufacture);
+
+        }
+
+        System.out.println("*********************************");
+        Car[] carSedan = new Car[4];
+        carSedan[0] = new Sedan("Chrysler", 200, 1990);
+        carSedan[1] = new Sedan("Opel", 190, 1999);
+        carSedan[2] = new Sedan("Cadillac", 220,1980);
+        carSedan[3] = new Sedan("Mercedes", 260, 1980);
+
+        for (int i = 0; i < carSedan.length; i++) {
+            Car car = carSedan[i];
+            System.out.println(car.models + " - " + car.maxSpeed + " - " + car.yearOfManufacture);
+
+
+        }
+
+//
+//        for (int i = 0; i < carSedan.length; i++) {
+//            System.out.println(carSedan[i].models);
+//
+//        }
+
+
+
+
+
+
+
 
 
 
