65714473d6e0afc617e245cbf4267ffd65db5018
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-06 10:30:53 +0200

    homework

---
+++ HomeWorkLesson6Task2/Developer.java
@@ -0,0 +1,25 @@
+package HomeWorkLesson6Task2;
+
+public class Developer extends Employee{
+
+    private String position;
+
+    public Developer(String name, int age, double salary, String position) {
+        super(name, age, salary);
+        this.position = position;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String report() {
+        setPosition("");
+        return super.report();
+    }
+}
---
+++ HomeWorkLesson6Task2/Main.java
@@ -0,0 +1,15 @@
+package HomeWorkLesson6Task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Developer developer =new Developer("Anton", 37, 2000.3, "Jav dev");
+
+       developer.report();
+
+       Employee employee = new Employee("Amiliy", 30, 2001.3);
+       employee.report();
+
+    }
+}
