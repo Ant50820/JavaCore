1b1a5e1e9992592cbdbd8b4214954907a074a153
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-07 00:02:24 +0200

    homework task 3 and Car

--- HoweWorkLesson5/Car.java
+++ HoweWorkLesson5/Car.java
@@ -1,6 +1,15 @@
 package HoweWorkLesson5;
 
+import PrcticalloopsArrays.Employee;
+
+import java.util.Scanner;
+
 public class Car {
+    /*
+    Create class Car with fields type,
+    year of production and engine capacity.
+    Create and initialize four instances of class Car.
+     */
     private int yearOfProduction;
     private int engineCapacity;
 
@@ -26,7 +35,48 @@ public class Car {
     }
 
 
+    /*
+    Display cars
+        * certain model year  (enter year in the console);
+        * ordered by the field year.
+     */
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int yearProd;
+
+        Car[] cars = new Car[4];
+        cars[0] = new Car(1980, 100);
+        cars[1] = new Car(1990, 150);
+        cars[2] = new Car(1960, 120);
+        cars[3] = new Car(1970, 190);
+
+        System.out.println(" Input year production ************");
+        yearProd = Integer.parseInt(scanner.nextLine());
+
+        for (Car car : cars) {
+            if (car.getYearOfProduction() == yearProd) {
+                System.out.println("Year of production: " + car.getYearOfProduction() +
+                        " and engine capacity - " + car.getEngineCapacity());
+            }
+        }
+        System.out.println("======================");
+        System.out.println();
+
+        Car tmp;
+        for (int i = 0; i < cars.length - 1; i++) {
+            for (int j = i + 1; j < cars.length; j++) {
+                if (cars[i].getYearOfProduction() > cars[j].getYearOfProduction()) {
+                    tmp = cars[i];
+                    cars[i] = cars[j];
+                    cars[j] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i].getYearOfProduction()
+                    + " and engine capacity - " + cars[i].getEngineCapacity());
+        }
     }
 }
