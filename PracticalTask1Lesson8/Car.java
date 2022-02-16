3714ccec97385034f0d170b3b3a3b6f932d2ecc0
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-02-16 22:07:38 +0200

    practical lesson 8

--- PracticalTask1Lesson8/Car.java
+++ PracticalTask1Lesson8/Car.java
@@ -1,6 +1,7 @@
 package PracticalTask1Lesson8;
 
-import java.time.LocalDate;
+import java.sql.Time;
+import java.time.*;
 
 public class Car {
 
@@ -8,7 +9,73 @@ public class Car {
     private LocalDate dataOfProduction;
     private double engineCapacity;
     private String color;
+    private int passengerCapacity;
     private boolean isAirConditioning;
 
+    public Car() {
 
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                "Model = '" + model + '\'' +
+                ", Data of production = " + dataOfProduction +
+                ", Engine Capacity = " + engineCapacity +
+                ", Color = '" + color + '\'' +
+                ", Passenger Capacity = " + passengerCapacity +
+                ", is Air Conditioning = " + isAirConditioning +
+                '}';
+    }
+
+    public static CarBuilder getCar(){
+        return new Car().new CarBuilder();
+    }
+
+    public class CarBuilder {
+
+        public CarBuilder(){
+            new Car();
+        }
+
+        public CarBuilder addModel (String model) {
+            Car.this.model = model;
+            return this;
+        }
+
+        public CarBuilder addColor(String color){
+            Car.this.color = color;
+            return this;
+        }
+
+        public CarBuilder addDataOfPdroduction(LocalDate local) {
+            Car.this.dataOfProduction = local;
+            return this;
+        }
+
+        public CarBuilder addEngineCapacity (double capacity) {
+            Car.this.engineCapacity = capacity;
+            return this;
+        }
+
+        public CarBuilder addPassengerCapacity (int cap) {
+            Car.this.passengerCapacity = cap;
+            return this;
+        }
+
+        public CarBuilder setIsConditioning(boolean air) {
+            Car.this.isAirConditioning = air;
+            return this;
+        }
+
+        public Car build() {
+            return Car.this;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car1 = Car.getCar().addModel("BNW")
+                .addEngineCapacity(100.2).addColor("Blue").addPassengerCapacity(5).setIsConditioning(true).build();
+        System.out.println(car1);
+    }
 }
