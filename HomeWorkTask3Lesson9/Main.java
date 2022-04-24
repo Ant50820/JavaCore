e9e292fc65fa1fdd2387e1923def45ebdaeb285e
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-04-24 12:09:07 +0300

    practic

---
+++ HomeWorkTask3Lesson9/Main.java
@@ -0,0 +1,16 @@
+package HomeWorkTask3Lesson9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Student student = new Student("Bob", 1);
+
+
+    }
+
+}
---
+++ PracticalExceptionLesson11/Main.java
@@ -0,0 +1,4 @@
+package PracticalExceptionLesson11;
+
+public class Main {
+}
