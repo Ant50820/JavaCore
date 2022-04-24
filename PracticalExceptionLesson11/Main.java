99c53442f4232b28db2afe5a43172e31d49137c6
Ant50820 <73773231+Ant50820@users.noreply.github.com>
2022-04-24 15:42:16 +0300

    practical

--- PracticalExceptionLesson11/Main.java
+++ PracticalExceptionLesson11/Main.java
@@ -1,4 +1,25 @@
 package PracticalExceptionLesson11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+try {
+    int i = 4; int j = 0;
+    System.out.println("Result: " + (i / j));
+} catch (Exception e) {
+    System.out.println("Division by zero");
 }
+
+
+//        int[] a = new int[];
+//        a[2] = 0;
+    }
+}
+
